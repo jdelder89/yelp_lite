31e2b866a530b12cabc7af57f6fbc464acd95073
Raghu Betina <raghu@firstdraft.com>
2016-11-29 03:45:21 +0000

    Direct association between states and cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,8 @@
 class City < ApplicationRecord
   # Direct associations
 
+  belongs_to :state
+
   # Indirect associations
 
   # Validations
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,6 +1,9 @@
 class State < ApplicationRecord
   # Direct associations
 
+  has_many   :cities,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
